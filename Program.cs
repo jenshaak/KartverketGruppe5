39635727cabc7b83fc80a04f77a7e49740d13161
Kevin Stratvert <jens.haakaas@gmail.com>
2024-10-30 16:47:45 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using KartverketGruppe5.Services;
+using KartverketGruppe5;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Bind the API settings from appsettings.json
+builder.Services.Configure<ApiSettings>(builder.Configuration.GetSection("ApiSettings"));
+
+// Register services and their interfaces
+builder.Services.AddHttpClient<IKommuneInfoService, KommuneInfoService>();
+builder.Services.AddHttpClient<IStedsnavnService, StedsnavnService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -8,8 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error"); 
     app.UseHsts();
 }
 
