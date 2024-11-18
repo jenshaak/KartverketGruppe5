36dbeba1fa51c642bd6c3ca7c33b12fc09382955
Larskyd <larsky@uia.no>
2024-11-18 11:22:59 +0100

    oppretta fylkemodell

---
+++ Models/Fylke.cs
@@ -0,0 +1,9 @@
+namespace KartverketGruppe5.Models
+{
+    public class Fylke
+    {
+        public int FylkeId { get; set; }
+        public string Navn { get; set; } = string.Empty;
+        public string FylkeNummer { get; set; } = string.Empty;
+    }
+} 
