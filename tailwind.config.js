280008b9beae9a35419908561e20ca14b2637927
sirijt <sirijt@uia.no>
2024-11-18 11:00:17 +0100

    la til tailwind-klasser

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,30 @@ module.exports = {
     './Pages/**/*.cshtml'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg': '#d0ecd6',
+        'primary': '#156630',
+        'secondary': '#156630',
+        'warning': '#ef4444',
+        'error': '#ef4444',
+        'custom-gray': '#2c2c2c',
+      }
+    },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /(bg|text|border|shadow)-.+/,
+    },
+    {
+      pattern: /(absolute|fixed|relative|hidden|h-screen|group|group-hover|cursor-pointer)/,
+    },
+    {
+      pattern: /(top|right)-(0|4|8|12|16)/,
+    },
+    {
+      pattern: /(line-clamp)-(0|1|2|3)/,
+    }
+  ]
 }
