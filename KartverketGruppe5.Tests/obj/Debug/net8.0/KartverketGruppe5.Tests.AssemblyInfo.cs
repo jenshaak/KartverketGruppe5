36ec2ca6d35ff967a5a1b8526c8222c4511835b7
jenshaak <jens.haakaas@gmail.com>
2024-11-25 03:53:42 +0100

    fiksa readme

--- KartverketGruppe5.Tests/obj/Debug/net8.0/KartverketGruppe5.Tests.AssemblyInfo.cs
+++ KartverketGruppe5.Tests/obj/Debug/net8.0/KartverketGruppe5.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KartverketGruppe5.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d1f544c0f94f1a15b66d64fb096d450f4af4c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec989d805c494d0dec75b0037f9871ea71b0f46f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KartverketGruppe5.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KartverketGruppe5.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KartverketGruppe5/Program.cs
+++ KartverketGruppe5/Program.cs
@@ -34,9 +34,6 @@ builder.Services.AddScoped<ISaksbehandlerRepository, SaksbehandlerRepository>();
 builder.Services.AddScoped<IInnmeldingRepository, InnmeldingRepository>();
 builder.Services.AddScoped<IKommunePopulateRepository, KommunePopulateRepository>();
 
-// Legg til DbContext som en service
-builder.Services.AddScoped<IDbContext, DbContext>();
-
 // Legg til services til containeren.
 builder.Services.AddControllersWithViews();
 
